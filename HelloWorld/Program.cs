2b4482233246b3e570e268d04fb899f6653ca84a
johnny82504171 <johnny82504171@gmail.com>
2020-11-29 08:41:15 +0900

    HelloWorld Output & argument check

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            if (args.Length == 0)
+            {
+                Console.WriteLine("HelloWorld");
+                return;
+            }
+
+            Console.WriteLine($"args count : {args.Length}");
+            
+            for (int i = 0; i <= args.Length - 1; i++)
+            {
+                Console.WriteLine($"args[{i}] info : {args[i]}");
+            }
+        }
+    }
+}
